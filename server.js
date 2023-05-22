339ed499c7fb122993fb9b85d1188d6bc85160ee
bellflufyb <conthicha.kjs24@gmail.com>
2023-05-22 11:44:49 +0700

    updatecode

---
+++ server.js
@@ -0,0 +1,100 @@
+// variable
+// var a = "cat";
+// let b = 12;
+// const c = 34.5;
+
+{
+    // console.log(a);
+    // console.log(b);
+    // console.log(c);
+    // a = "bat";
+    // b = 13;
+    // c = 35:
+    // console.log(a);
+    // console.log(b);
+}
+
+// console.log(a);
+// console.log(b);
+
+// type variable
+var name = "chonthicha";
+var id = "630410016"
+
+var a = "1";
+var b = "2";
+console.log(a + b);
+
+var a = 1;
+var b = 2;
+console.log(a + b);
+
+// array
+var c = ["chonthicha", 630410016, "cmu"]
+console.log(c[0]);
+
+
+
+
+// object
+var d = {
+    name: "chonthicha",
+    id: 56789,
+    university: "cmu"
+}
+console.log(d["id"]);
+console.log(d.university)
+
+// array object
+var e = [
+    { Name: "guitar", id: 1 },
+    { Name: "bee", id: 2 },
+    { Name: "F", id: 3 },
+    { Name: "Pan", id: 4 }
+]
+console.log(e[1].Name);
+
+// loop
+// foreach
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// arr.forEach(i => console.log(i));  
+
+// for loop
+for (var i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+// while loop
+var i = 1;
+while (i <= 10) {
+    console.log("while", i);
+    i += 1
+}
+
+// if condition
+
+var f = 1
+if (f == 0) {
+    console.log("yes")
+
+}
+if (f == 0) {
+    console.log("yes")
+} else {
+    console.log("no")
+}
+var value = 80;
+if (value <= 50) { console.log("A") }
+else if (value <= 60) { console.log("C") }
+
+else if (value <= 79) { console.log("B") }
+
+else { console.log("A") }
+
+
+f == 0 ? console.log("yes") : console.log("no")
+
+
+
+
+
